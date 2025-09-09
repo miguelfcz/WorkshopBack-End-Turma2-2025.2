76fc4d377975ba2d7d089f9c36e347ccf074384b
Miguel <miguelcf2006@gmail.com>
2025-09-09 14:54:25 -0300

    :arrow_up: Adicionando requiremets e app2 do Django

--- django_workshop/app/models.py
+++ django_workshop/app/models.py
@@ -1,3 +1,9 @@
 from django.db import models
 
-# Create your models here.
+class pessoa(models.Model):
+    nome = models.CharField(max_length=100)
+    idade = models.IntegerField()
+    email = models.EmailField(unique=True)
+
+    def __str__(self):
+        return self.nome
--- django_workshop/app2/urls.py
+++ django_workshop/app2/urls.py
@@ -2,5 +2,5 @@ from django.urls import path
 from . import views
 
 urlpatterns = [
-    path('home1/', views.home, name='home'),
+    path('home2', views.home2, name='home2'),
 ]
