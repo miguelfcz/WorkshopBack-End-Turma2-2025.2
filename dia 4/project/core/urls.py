f1ae24de21854621b22b41abaf2557b9a47a4734
Miguel <miguelcf2006@gmail.com>
2025-09-10 19:41:53 -0300

    :arrow_up: Adicionando modificações do Projeto com CRUD

--- dia 4/project/core/urls.py
+++ dia 4/project/core/urls.py
@@ -2,6 +2,9 @@ from django.urls import path
 from . import views
 
 urlpatterns = [
-    path('home/', views.home, name = 'home'),
-    path('endereco/', views.buscar_cep, name = 'endereco'),
+    path('base/', views.base, name = 'base'),
+    path('buscarcep/', views.BuscarCepFormView.as_view(), name = 'buscarcep'),
+    path('listar/', views.EnderecoListView.as_view(), name = 'listar'),
+    path('detalhe/<int:pk>/', views.EnderecoDetailView.as_view(), name = 'detalhe'),
+    path('delete/<int:pk>/', views.EnderecoDeleteView.as_view(), name = 'delete'),
 ]
--- dia 4/project/core/views.py
+++ dia 4/project/core/views.py
@@ -1,38 +1,57 @@
 from django.shortcuts import render
 from django.http import HttpResponse
+from django.urls import reverse_lazy
 from .models import Endereco
 from .forms import EnderecoForm
+from django.views.generic import FormView, ListView, DetailView, DeleteView
 import requests
 
-def home(request):
-    return render(request, 'home.html')
+def base(request):
+    return render(request, 'base.html')
+#FormView de via cep
+class BuscarCepFormView(FormView):
+    template_name = 'enderecoFormView.html'
+    form_class = EnderecoForm
+    success_url = 'buscarcep/'
 
-def buscar_cep(request):
-    form = EnderecoForm(request.GET or None) #Pega os dados do formulário
-    endereco = None
-    erro = None
+    def form_valid(self,form):
+        cep = form.cleaned_data['cep']
 
-    if form.is_valid():
-        cep = form.cleaned_data['cep'] #Faz a limpeza dos dados
         try:
             response = requests.get(f'https://viacep.com.br/ws/{cep}/json/')
+            response.raise_for_status()
+            data = response.json()
 
-            if response.status_code == 200:
-                data = response.json()
-
-                if data.get("erro"):
-                    return render(request, 'endereco.html', {'erro': data.get("erro")})
-                else:
-                    endereco = Endereco(
-                        rua = data.get('logradouro', ''),
-                        bairro = data.get('bairro', ''),
-                        cidade = data.get('localidade', ''),
-                        estado = data.get('uf', ''),
-                        cep = data.get('cep', '')
-                    )
-                    endereco.save()
+            if data.get("erro"):
+                form.add_error('cep', 'CEP não encontrado.')
+                return self.form_invalid(form)
+            else:
+                endereco = Endereco(
+                    rua=data.get('logradouro', ''),
+                    bairro=data.get('bairro', ''),
+                    cidade=data.get('localidade', ''),
+                    estado=data.get('uf', ''),
+                    cep=data.get('cep', '')
+                )
+                endereco.save()
 
         except requests.exceptions.RequestException:
-            erro = f"Erro ao buscar o CEP: {cep}"
-            
-    return render(request, 'endereco.html', {'endereco': endereco})
+            form.add_error('cep', 'Erro ao buscar o CEP.')
+            return self.form_invalid(form)
+
+        return render(self.request, self.template_name, {'form': form, 'endereco': endereco})
+
+class EnderecoListView(ListView):
+    model = Endereco
+    template_name = 'enderecoListView.html'
+    context_object_name = 'enderecos'
+
+class EnderecoDetailView(DetailView):
+    model = Endereco
+    template_name = 'enderecoDetailView.html'
+    context_object_name = 'endereco'
+
+class EnderecoDeleteView(DeleteView):
+    model = Endereco
+    template_name = 'enderecoDeleteView.html'
+    success_url = reverse_lazy('listar')
